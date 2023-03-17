d8065aa3451eeb5bc4920b34ddafe4174aa0b109
DanielRendon1020 <d.danny1020@gmail.com>
2023-03-16 20:42:36 -0700

    fetch books and list them

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,23 @@
+async function main(){
+    let respose = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
 
+    console.log(books)
+
+    books.forEach(function(book){
+
+        let bookContainer = document.querySelector('#root')
+
+        let li = document.createElement('li')
+        li.textContent = book.title
+
+        
+
+        
+
+        
+    })
+}
+
+main()
 // Your Code Here
